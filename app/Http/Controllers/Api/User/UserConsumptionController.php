194a98499510ddebee155e11c6bab7012f27b58d
Lakshan-Madushanka <Lakshan-Madushanka@users.noreply.github.com>
2022-07-05 06:45:25 +0000

    Fix styling

--- app/Http/Controllers/Api/User/UserConsumptionController.php
+++ app/Http/Controllers/Api/User/UserConsumptionController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api\User;
 use App\Actions\UserConsumption\CreateNewConsumption;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\FuelConsumeRequest;
-use App\Models\Consumption;
 
 class UserConsumptionController extends Controller
 {
